06da91be6d8526e328627afe21471c43b1971b2b
EunjungSong00 <unjyon.song@gmail.com>
2022-07-24 23:18:34 +0900

    prisma and planetscale setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,15 @@
 //components - 어디서 쓸지 정해줌
 module.exports = {
   content: [
-      // "./pages/**/*.{js, jsx, ts, tsx}",
-      "./apps/site/pages/**/*.{js, jsx, ts, tsx}",
-      "./components/**/*.{js, jsx, ts, tsx}"
+      // "./pages/**/*.{js,jsx,ts,tsx}",
+      "./pages/**/*.{js,jsx,ts,tsx}",
+      "./components/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "media",
+  plugins: [
+    require("@tailwindcss/forms")
+  ],
 }
